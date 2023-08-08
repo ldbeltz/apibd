fb456b89eb1b717cadec80672d102d848b66754d
Humberto <humbertocardoso.lima@sead.se.gov.br>
2023-08-08 16:56:40 -0300

    add methods

--- src/main/java/com/ufs/apibd/controllers/ApiController.java
+++ src/main/java/com/ufs/apibd/controllers/ApiController.java
@@ -1,5 +1,14 @@
 package com.ufs.apibd.controllers;
 
-public class ApiController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/usuario")
+public class ApiController {
+    @PostMapping
+    public ResponseEntity<Object> cadastraUsuario(){
+
+    }
 }
